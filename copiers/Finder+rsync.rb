9347a6d79a472448d17ddca3eaed939f4dcd7d2b
Lifepillar <github@lifepillar.com>
2013-01-23 16:39:26 +0100

    Remove chflags in Finder+rsync, too.

--- copiers/Finder+rsync.rb
+++ copiers/Finder+rsync.rb
@@ -5,7 +5,6 @@ synchronizes the data with Homebrew'rsync (/usr/local/bin/rsync).
 These are the commands performed by this task:
 
   tell app "Finder" to duplicate the entire contents of <source> to <target>
-  sudo chflags -R nouchg,noschg,nosappend <target>
   sudo /usr/local/bin/rsync -aNHAX --itemize-changes --super
     --protect-args --fileflags --force-change <source>/ <target>
 =end
@@ -23,18 +22,8 @@ task :clone do
     begin
       run_baby_run 'osascript', ['-e', setsource, '-e', setdest, '-e', script],
         :sudo => true, :verbose => false, :err => '/dev/null'
-    rescue => ex
-      puts "Finder+rsync clone task has exited with errors: #{ex}"
-    end
-
-    # Unlocking files should not be needed, given --force-change,
-    # but rsync 3.0.7 produces errors if the following is omitted.
-    puts '     rsyncing...'
-    begin
-      run_baby_run('chflags', ['-R', 'nouchg,noschg,nosappend', target_path],
-        :sudo => true, :verbose => false)
     rescue
-      puts "Finder+rsync chflags has failed: #{ex}"
+      puts "Finder has exited with errors, but rsync may fix them."
     end
     rsync = '/usr/local/bin/rsync'
     args = ['-aNHAX']
@@ -45,9 +34,10 @@ task :clone do
     source_path += '/'
     args << source_path << target_path
     begin
-      run_baby_run(rsync, args, :sudo => true, :verbose => false)
+      run_baby_run rsync, args, :sudo => true, :verbose => false,
+        :redirect_stderr_to_stdout => true
     rescue
-      puts "Finder+rsync clone task has failed: #{ex}"
+      puts "rsync has exited with errors. Some files may not have been copied correctly."
     end
   rescue Rbb::DiskImageExists
     puts 'Skipping Finder+rsync clone task (volume exists).'
@@ -66,18 +56,10 @@ task :copy do
     script    = 'tell application "Finder" to duplicate the entire contents ' +
                 'of source to dest with replacing'
     begin
-      run_baby_run 'osascript', ['-e', setsource, '-e', setdest, '-e', script], :sudo => true
-    rescue => ex
-      puts "Finder+rsync clone task has failed: #{ex}"
-    end
-  
-    # Unlocking files should not be needed, given --force-change,
-    # but rsync 3.0.7 produces errors if the following is omitted
-    begin
-      run_baby_run('chflags', ['-R', 'nouchg,noschg,nosappend', target_path],
-        :sudo => true, :verbose => false)
+      run_baby_run 'osascript', ['-e', setsource, '-e', setdest, '-e', script],
+        :sudo => true, :verbose => false, :err => '/dev/null'
     rescue
-      puts "Finder+rsync chflags has failed: #{ex}"
+      puts "Finder has exited with errors, but rsync may fix them."
     end
     rsync = '/usr/local/bin/rsync'
     args = ['-aNHAX']
@@ -85,12 +67,12 @@ task :copy do
     args << '--itemize-changes' << '--super'
     args << '--fileflags' << '--force-change' << '--crtimes'
     args << "--rsync-path=#{rsync}" << '--quiet'
-    source_path += '/'
-    args << source_path << target_path
+    args << $source.to_s + '/' << target
     begin
-      run_baby_run(rsync, args, :sudo => true, :verbose => false)
+      run_baby_run rsync, args, :sudo => true, :verbose => false,
+        :redirect_stderr_to_stdout => true
     rescue
-      puts "Finder+rsync clone task has failed: #{ex}"
+      puts "rsync has exited with errors. Some files may not have been copied correctly."
     end
   end
 end
